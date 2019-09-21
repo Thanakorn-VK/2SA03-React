669935757bbe4f195681f46e9c6d7de41a1c9e1a
Thanakorn-VK <47586184+Thanakorn-VK@users.noreply.github.com>
2019-09-22 01:34:49 +0700

    test06
    ทำการสร้างปุุมสำหรับเลือกหมวกหมู่

--- card-game/src/App.js
+++ card-game/src/App.js
@@ -3,13 +3,18 @@ import './App.css'
 import CharacterCard from'./CharacterCard';
 import WordCard from'./WordCard';
 
-const word = "Hello";
+const word = "";
 class App extends Component {
   render() {
     return (
       <div>
-        <WordCard value="hello"/>
+        <h1 class="setcenter">
+          <button class="button1">    ANIMALS    </button>
+          <button class="button2">    FRUIT    </button>
+          <button class="button3">    FOOD    </button>
+        </h1>
       </div>
+//<WordCard value="hello"/>
     )
   }
 }
