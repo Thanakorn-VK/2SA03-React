6bbb68ead35d78216b5ee74e99e9e89f9b67abf2
Thanakorn-VK <47586184+Thanakorn-VK@users.noreply.github.com>
2019-09-23 00:46:03 +0700

    final
    เพิ่มการแสดงรูปภาพของประโยคเพื่อเป็นคำใบ้

--- card-game/src/App.js
+++ card-game/src/App.js
@@ -7,15 +7,28 @@ var categories = ['ONEPIECE', 'NARUTO', 'DORAEMON', 'GINTAMA'];
 var randomnum = Math.floor(Math.random()*categories.length)
 var word = categories[randomnum];
 
+if(randomnum == 0) {
+  var image = "ONEPIECE.jpg";
+}
+if(randomnum == 1) {
+  var image = "NARUTO.jpg";
+}
+if(randomnum == 2) {
+  var image = "DORAEMON.jpg";
+}
+if(randomnum == 3) {
+  var image = "GINTAMA.jpg";
+}
+
+
 class App extends Component {
   render() {
     
     return (
       <div class="setcenter">
         <h1>CardGame</h1>
-
+        <img src = {image}></img>
         <WordCard value={word}/>
-        <h1> </h1>
         <p>By THANAKORN VIENGKAE 6010110136</p>
       </div> 
       )
