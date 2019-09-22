3c41652bfe12fd26d2363332c9004b8d260bd940
Thanakorn-VK <47586184+Thanakorn-VK@users.noreply.github.com>
2019-09-22 15:41:07 +0700

    test09
    จัดเรียงหน้าให้เป็นระเบียบมากยิ่งขึ้น

--- card-game/src/App.js
+++ card-game/src/App.js
@@ -5,14 +5,24 @@ import WordCard from'./WordCard';
 
 const word = "HELLO";
 class App extends Component {
-  
   render() {
     
     return (
-      <div>
+      /*<div>
+        <h1 class="setcenter">
+          
+          <button class="button1">    ANIMALS    </button>
+          <button class="button2">    FRUIT    </button>
+          <button class="button3">    FOOD    </button>
+          
+        </h1>
+      </div>*/
+      <div class="setcenter">
+        <h1>6010110136 - CardGame</h1>
+        
         <WordCard value={word}/>
       </div>
-    )
+      )
   }
 }
 
